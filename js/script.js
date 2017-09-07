7a10fe210887f8990388996b3318b001cba01805
tagurkova <tatsiana.agurkova@artezio.com>
2017-09-07 16:25:02 +0300

    Ignore idea files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $('.comments').on('click', function(event) {
       var $target = $(event.target);
-    if($target.hasClass('reply')) {
+      if($target.hasClass('reply')) {
         event.preventDefault();
         // if()
         var newCommentForm = new CommentForm(false);
         $target.closest('.comment').append(newCommentForm.getFormElement());
-    }
+      }
   });
   // var mainCommentForm = new CommentForm(true);
   // $onReply =
